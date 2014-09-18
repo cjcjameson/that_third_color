b7994059e18eccb61e64acf66abe1a66dac45bc2
C.J. Jameson <cjcjameson@gmail.com>
2014-09-18 14:59:56 -0700

    New styling, input working, javascript shell
     - Javascript helper functions still need to be written
     - Styling looks ugly still

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,3 +1,23 @@
 window.onload = function(){
-	alert("loaded");
+	
+	$(".submit").click(generateThirdColor);
+}
+
+var generateThirdColor = function() {
+	var inputColors = $("form").serializeArray();
+	var thirdColor = calculateThirdColor(inputColors[0].value, inputColors[1].value);
+	$(".output").css("visibility","visible");
+	$(".output").html(thirdColor);
+	$(".output").css("background-color",thirdColor);
+	if (isLight(thirdColor)) {
+		$(".output").css("color","#000000");
+	}
+}
+
+var calculateThirdColor = function(color1, color2) {
+	return "#dddd99";
+}
+
+var isLight = function(color) {
+	return true;
 }
