92b607f9f6df3693c8b0d6934b18bab658df901c
C.J. Jameson <cjcjameson@gmail.com>
2014-09-18 15:13:18 -0700

    refactor into a view method

--- scripts/colors.js
+++ scripts/colors.js
@@ -5,17 +5,23 @@ window.onload = function(){
 
 var generateThirdColor = function() {
 	var inputColors = $("form").serializeArray();
-	var thirdColor = calculateThirdColor(inputColors[0].value, inputColors[1].value);
+	var color1 = inputColors[0].value;
+	var color2 = inputColors[1].value;
+	var color3 = calculateThirdColor(color1, color2);
+	showThirdColor(color3);
+}
+
+var calculateThirdColor = function(color1, color2) {
+	return "#dddd99";
+}
+
+var showThirdColor = function(color) {
 	$(".output").css("visibility","visible");
 	$(".output").html(thirdColor);
 	$(".output").css("background-color",thirdColor);
 	if (isLight(thirdColor)) {
 		$(".output").css("color","#000000");
 	}
-}
-
-var calculateThirdColor = function(color1, color2) {
-	return "#dddd99";
 }
 
 var isLight = function(color) {
